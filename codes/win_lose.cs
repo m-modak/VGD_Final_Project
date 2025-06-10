c6adcf62b01447cbe73b71316c10fe84dda06607
m-modak <sarahm046@stemcollegiate.ca>
2025-06-10 13:13:08 -0600

    Update win_lose.cs

--- codes/win_lose.cs
+++ codes/win_lose.cs
@@ -1,1 +1,25 @@
-//add when the win and lose scenes open, just add code im so tired
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WinScript : Monobehaviour 
+{
+  void Start()
+  {
+  
+  }
+  void Update()
+  {
+  
+  }
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("Player"))
+      {
+        SceneManager.LoadScene(1);
+      }
+  }
+  
+}
+
+  
