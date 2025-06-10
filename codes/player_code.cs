e88b8acbd79f3d709e0bdb8a6793c9683d0bd775
m-modak <sarahm046@stemcollegiate.ca>
2025-06-10 12:49:49 -0600

    Update player_code.cs

--- codes/player_code.cs
+++ codes/player_code.cs
@@ -1,1 +1,42 @@
-//add player code, change player jump thing
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float direction = 0f;
+    private Rigidbody2D player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        if (direction > 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else if (direction < 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        if (Input.GetButtonDown(Keycode.Space)) 
+        //I changed the button needed for the jump function because i hate using the arrow keys only. i think using two different hands makes it easier to play the game. 
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+        }
+    }
+}
